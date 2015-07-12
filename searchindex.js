42e9e8b7e549255784e124d80a89c21e4238c827
Iblis Lin <e196819@hotmail.com>
2015-07-12 09:41:52 +0800

    Update documentation

--- searchindex.js
+++ searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Iblis&#8217;s Notes&#8217;s documentation!"],titleterms:{document:0,ibli:0,indic:0,note:0,tabl:0,welcom:0}})
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:[],page:0,search:0},titles:["Notes&#8217;s"],titleterms:{document:[],ibli:[],indic:0,note:0,tabl:0,welcom:[]}})
